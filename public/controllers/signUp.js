b4aed6c6db868d8f524844ec9044db77bfbb89d1
dilipkumar0 <dilipkundrapu123@gmail.com>
2017-04-19 16:19:32 +0530

    Add files via upload

---
+++ public/controllers/signUp.js
@@ -0,0 +1,7 @@
+
+    var app =angular.module('myModule',[]);
+    app.controller('signUpCtrl' ,function ($scope, $state) {
+    $scope.name = 'rajesh' ;
+        
+        console.log('rajesh and ...')
+    })
